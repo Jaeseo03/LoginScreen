be7437c02b223921b8b54d137127f5e29f1b7e64
재서 김 <23010114@suwon.ac.kr>
2026-04-02 12:49:50 +0900

    과제2 코딩과 테스트 완료
    메시지박스 대신 로그인 화면에 로그인 실패 문구 뜨게 만들기 구현 완

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,14 +32,18 @@
             txtID = new TextBox();
             txtPW = new TextBox();
             btnLogin = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            lblErrorMsg = new Label();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // lblAppName
             // 
+            lblAppName.Anchor = AnchorStyles.None;
             lblAppName.AutoSize = true;
             lblAppName.Font = new Font("맑은 고딕", 36F, FontStyle.Regular, GraphicsUnit.Point, 129);
             lblAppName.ForeColor = SystemColors.HotTrack;
-            lblAppName.Location = new Point(141, 30);
+            lblAppName.Location = new Point(124, 38);
             lblAppName.Name = "lblAppName";
             lblAppName.Size = new Size(186, 81);
             lblAppName.TabIndex = 0;
@@ -47,10 +51,12 @@
             // 
             // txtID
             // 
+            txtID.Anchor = AnchorStyles.None;
+            txtID.Font = new Font("맑은 고딕", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 129);
             txtID.ForeColor = Color.Silver;
-            txtID.Location = new Point(94, 146);
+            txtID.Location = new Point(46, 171);
             txtID.Name = "txtID";
-            txtID.Size = new Size(273, 27);
+            txtID.Size = new Size(343, 38);
             txtID.TabIndex = 2;
             txtID.Text = "아이디";
             txtID.FontChanged += txtID_FontChanged;
@@ -60,10 +66,12 @@
             // 
             // txtPW
             // 
+            txtPW.Anchor = AnchorStyles.None;
+            txtPW.Font = new Font("맑은 고딕", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 129);
             txtPW.ForeColor = Color.Silver;
-            txtPW.Location = new Point(94, 242);
+            txtPW.Location = new Point(46, 238);
             txtPW.Name = "txtPW";
-            txtPW.Size = new Size(273, 27);
+            txtPW.Size = new Size(343, 38);
             txtPW.TabIndex = 3;
             txtPW.Text = "패스워드";
             txtPW.Enter += txtPW_Enter;
@@ -72,10 +80,11 @@
             // 
             // btnLogin
             // 
+            btnLogin.Anchor = AnchorStyles.None;
             btnLogin.BackColor = Color.DarkOrange;
             btnLogin.Font = new Font("맑은 고딕", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 129);
             btnLogin.ForeColor = SystemColors.ControlText;
-            btnLogin.Location = new Point(151, 315);
+            btnLogin.Location = new Point(144, 364);
             btnLogin.Name = "btnLogin";
             btnLogin.Size = new Size(146, 58);
             btnLogin.TabIndex = 1;
@@ -83,19 +92,52 @@
             btnLogin.UseVisualStyleBackColor = false;
             btnLogin.Click += btnLogin_Click;
             // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.Controls.Add(lblAppName, 1, 0);
+            tableLayoutPanel1.Controls.Add(txtID, 1, 1);
+            tableLayoutPanel1.Controls.Add(txtPW, 1, 2);
+            tableLayoutPanel1.Controls.Add(btnLogin, 1, 4);
+            tableLayoutPanel1.Controls.Add(lblErrorMsg, 1, 3);
+            tableLayoutPanel1.Location = new Point(12, 12);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 35F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 15F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Size = new Size(437, 450);
+            tableLayoutPanel1.TabIndex = 4;
+            // 
+            // lblErrorMsg
+            // 
+            lblErrorMsg.Anchor = AnchorStyles.None;
+            lblErrorMsg.AutoSize = true;
+            lblErrorMsg.Font = new Font("맑은 고딕", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblErrorMsg.ForeColor = Color.Red;
+            lblErrorMsg.Location = new Point(54, 302);
+            lblErrorMsg.Name = "lblErrorMsg";
+            lblErrorMsg.Size = new Size(327, 23);
+            lblErrorMsg.TabIndex = 4;
+            lblErrorMsg.Text = "아이디 또는 비밀번호가 잘못 되었습니다.";
+            lblErrorMsg.Visible = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(458, 450);
-            Controls.Add(btnLogin);
-            Controls.Add(txtPW);
-            Controls.Add(txtID);
-            Controls.Add(lblAppName);
+            ClientSize = new Size(461, 474);
+            Controls.Add(tableLayoutPanel1);
             Name = "Form1";
             Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -104,5 +146,7 @@
         private TextBox txtID;
         private TextBox txtPW;
         private Button btnLogin;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label lblErrorMsg;
     }
 }
